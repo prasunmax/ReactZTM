3f9c4a741867afa0cdc0ab74fa9e9fe31fc62e7e
Prasun Das <prasunmax@gmail.com>
2020-09-14 00:46:35 +0530

    Componentizations of Home page and menuitem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import HomePage from './pages/homepage/homepage.component';
+
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return <div className='App'>
+      <HomePage />
+    </div>;
   }
 }
 
