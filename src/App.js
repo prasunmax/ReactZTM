a329d5f48c931011d3b9c68da1c3f2595343b2c5
Prasun Das <prasunmax@gmail.com>
2020-09-15 01:14:36 +0530

    added collection item and collection preview

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
+
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 class App extends Component {
   render() {
     return <div className='App'>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>;
   }
