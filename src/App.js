9a878bbcbe186c03c3ea26c62fcdc9bf06845fd5
Prasun Das <prasunmax@gmail.com>
2020-09-15 01:32:32 +0530

    added Header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
 
+import Header from './components/header/header.component';
+
 
 const HatsPage = () => (
   <div>
@@ -14,6 +16,7 @@ const HatsPage = () => (
 class App extends Component {
   render() {
     return <div className='App'>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
