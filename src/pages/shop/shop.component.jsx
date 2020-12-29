f9515ed93f8321656f06bd1b710e979ae1f6553c
Prasun Das <prasunmax@gmail.com>
2020-12-29 11:03:48 +0530

    Completed routing the items in one way

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-
+import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collection-overview/collection-overview.component';
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({ collections }) => (
+
+
+const ShopPage = ({ match  }) => (
   <div className="shop-page">
-      <CollectionsOverview/>
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
